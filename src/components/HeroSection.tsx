2ed9f8b82b9ab254d46c46c2e8e486b580e2a353
Alan <alanroit@gmail.com>
2025-12-10 01:05:17 -0300

    Update CTA text to 'Become a GTM partner!'

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -59,7 +59,7 @@ const HeroSection = () => {
             className="bg-white hover:bg-gray-50"
           >
             <button onClick={() => scrollToSection("gtm-partners")}>
-              Become a GTM partner NOW NOW NOW  
+              Become a GTM partner!
             </button>
           </Button>
         </div>
