b13f21fde080558b0373ee6fb1f04c80b412df40
Alan <alanroit@gmail.com>
2025-12-10 00:38:39 -0300

    Mi mensaje de commit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -59,7 +59,7 @@ const HeroSection = () => {
             className="bg-white hover:bg-gray-50"
           >
             <button onClick={() => scrollToSection("gtm-partners")}>
-              Become a GTM partner NOW
+              Become a GTM partner NOW NOW
             </button>
           </Button>
         </div>
